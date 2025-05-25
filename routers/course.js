e771d077081ad2d8607db2ef54e0095ac0ba89d8
tomotele <tomotelechristopher25@gmail.com>
2025-05-25 10:29:14 +0100

    chore:Add cloudinary

--- routers/course.js
+++ routers/course.js
@@ -1,11 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { createCourse, getCourses } = require("../controllers/auth-controllers");
-const { uploadImage } = require("../controllers/upload-controller");
-
-const upload = require("../middleware/upload");
 
 router.get("/getCourses", getCourses);
-router.post("/upload-image", uploadImage);
+
 router.post("/createCourse", createCourse);
 module.exports = router;
